41b2edde9a6a8562abd1d1b368728ba3b22cb95b
Nguyễn Chí Hiếu <hieuchnguyen@live.com>
2020-08-27 05:21:54 +0700

    Add project files.

---
+++ Lab01Review/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab01Review
+{
+    class Program
+    {
+        //https://ideone.com/eGhXKB
+        /// <summary>
+        /// Buoc 1. Khai bao va dinh nghia lop Student gom 4 thuoc tinh
+        /// Id, Fullname, Birthday, Gender
+        /// </summary>
+        public class Student
+        {
+            public int Id;
+            public string Fullname;
+            public string Birthday;
+            public bool Gender;
+        }
+
+        /// <summary>
+        /// Buoc 2. Su dung lop Student
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // Khai bao va khoi tao gia tri
+            Student std = new Student();
+            std.Id = 100;
+            std.Fullname = "Nguyen";
+            std.Birthday = "1990";
+            std.Gender = true;
+
+            // Debug: nhan F9 tai dong lenh dung-> nhan F5 debug
+            Console.WriteLine("{0}, {1}, {2}, {3}",
+                std.Id,
+                std.Fullname,
+                std.Birthday,
+                std.Gender);
+        }
+    }
+}
---
+++ Lab02Recursion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02Recursion
+{
+    class Program
+    {
+        // https://ideone.com/Bo4Bkp
+        static long factorial(int n)
+        {
+            if (n <= 1)
+                return 1;            
+            return n * factorial(n - 1);
+        }
+
+        static int gcd(int m, int n)
+        {
+            int r = m % n;
+            if (r == 0) 
+                return n;
+            return gcd(n, r);
+        }
+
+        static void Main(string[] args)
+        {
+            int n = 3;
+            Console.WriteLine(factorial(n)); // 1 * 2 * 3 = 6
+
+            Console.WriteLine(gcd(15, 45));
+        }
+    }
+}
